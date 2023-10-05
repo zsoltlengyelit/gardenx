2eb2080190dd8f034c569044e4913a5809edb7ff
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2023-10-05 13:34:22 +0200

    make more simple code

--- frontend/src/common/useControllerColor.ts
+++ frontend/src/common/useControllerColor.ts
@@ -4,17 +4,18 @@ import { useMemo } from 'react';
 
 const colors = [
   'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'neutral', 'error',
-  'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'neutral', 'error',
-  'primary', 'secondary', 'accent', 'info', 'success', 'warning', 'neutral', 'error',
 ].map(color => `bg-${color}`);
 
 export function useControllerColor() {
   const { controllers } = useLiveState();
-
   const allControllers = useMemo(() => controllers.map(c => c.controller), [controllers]);
   return (controller: Controller) => {
+    let baseColors = [...colors];
+    while (baseColors.length < controllers.length) {
+      baseColors = [...baseColors, ...colors];
+    }
     const index = allControllers.map(c => c.id).indexOf(controller.id);
 
-    return colors[index];
+    return baseColors[index];
   };
 }
