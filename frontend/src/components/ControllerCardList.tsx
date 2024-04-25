3d3d1fffeea3b58bc5254e37b1121a6bd66045cb
Zsolt Lengyel <zsolt.lengyel.it@gmail.com>
2024-04-25 22:12:56 +0200

    All off / all auto feature

--- frontend/src/components/ControllerCardList.tsx
+++ frontend/src/components/ControllerCardList.tsx
@@ -1,9 +1,12 @@
 import ControllerCard from './ControllerCard';
-import { ControllerChange } from '../api/types';
+import { ControllerChange, OnOffAuto } from '../api/types';
 import NewControllerCard from './NewControllerCard';
 import { useAtomValue } from 'jotai';
 import { editorModeAtom } from '../atoms';
 import { useControllerColor } from '../common/useControllerColor';
+import { Button, Card } from 'react-daisyui';
+import React from 'react';
+import { AllControllerCard } from './AllControllerCard';
 
 type Props = {
     controllers: ControllerChange[];
@@ -17,6 +20,9 @@ export default function ControllerCardList({ controllers }: Props) {
   return (
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4 py-4">
+
+            <AllControllerCard controllerChanges={controllers}/>
+
             {controllers.map(({ controller, set }, index) => (
                 <ControllerCard
                     key={controller.id}
@@ -26,7 +32,7 @@ export default function ControllerCardList({ controllers }: Props) {
                 />
             ))}
 
-            {editorMode && <NewControllerCard />}
+            {editorMode && <NewControllerCard/>}
         </div>
   );
 }
